ac07f59e1c41a3bfc1f9a332fd43cb1c01bb4258
PaolaGuedes <paolaguedesbacca2002@gmail.com>
2021-10-29 02:40:40 -0300

    handleRfresh and handleRemove

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Header from './Header'
 import Post from './Post'
 
-const posts = [
-	{ title: 'Titulo1', subtitle: 'Subtitulo 1', likes: 20},
-	{ title: 'Titulo2', subtitle: 'Subtitulo 2', likes: 10},
-	{ title: 'Titulo3', subtitle: 'Subtitulo 3', likes: 50},
-]
-
 function App() {
+
+	const [posts, setPosts] = useState([		
+		{ id: Math.random(), title: 'Titulo1', subtitle: 'Subtitulo 1', likes: 20},
+		{ id: Math.random(), title: 'Titulo2', subtitle: 'Subtitulo 2', likes: 10},
+		{ id: Math.random(), title: 'Titulo3', subtitle: 'Subtitulo 3', likes: 50},
+		])
+
+	function handleRefresh(){
+		setPosts((prevState)=>[
+			...prevState,
+			{ 
+				id: Math.random(),
+				title: `Titulo#0${prevState.length+1}`,
+				subtitle: `Subtitulo#0${prevState.length+1}`,
+				likes: 50
+			}
+		])
+	}
+
+	function handleRemove(postId){
+		setPosts((prevState)=>(
+			prevState.filter(post => post.id != postId)
+		))
+	}
 	return (
 	<>
 	<Header>
-		<ul>
-			<li><a href="#">oi</a></li>
-			<li><a href="#">oi</a></li>
-			<li><a href="#">oi</a></li>
-		</ul>
+		<h2>
+			posts da semana
+			<button onClick={handleRefresh}>atualizar</button>
+		</h2>
 	</Header>
 		{
 			posts.map( post => (
 				<Post 
-					key={post.title}
+					key={post.id}
+					onRemove={handleRemove}
 					likes={post.likes}
 					post={{
+						id: post.id,
 						title: post.title,
 						subtitle: post.subtitle,
 					}}	
--- src/Post.js
+++ src/Post.js
@@ -6,6 +6,9 @@ function Post(props){
         <>
         <article>
         <strong>{props.post.title}</strong>
+        <button onClick={() => props.onRemove(props.post.id)}>
+            Remover
+        </button>
         <br/>
         <small>{props.post.subtitle}</small>
         <br/>
@@ -20,7 +23,9 @@ export default Post
 
 Post.propTypes = {
     likes: PropTypes.number.isRequired,
+    onRemove: PropTypes.func.isRequired,
     post: PropTypes.shape({
+        id: PropTypes.number.isRequired,
         title: PropTypes.string.isRequired,
         subtitle: PropTypes.string.isRequired,
     }).isRequired,
